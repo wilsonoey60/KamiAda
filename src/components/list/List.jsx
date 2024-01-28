eb4b436d47952b40efa8c29d183a890f6a3a1c2d
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-01-28 19:16:49 +0800

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,18 +2,13 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ArticleCard from '../card/article-card';
 import { showFormattedDateTime } from '../../utils/date';
-import { getServicebyID } from '../../utils/api';
  
 function List({ services }) {
-  const [UserService, setUserService] = useState('');
   if (services.length >= 0) {
     return (
       <>
         {
           services.map((service) => (
-            // getServicebyID(service.idservice).then(({ data }) => {
-            //   setUserService(data);
-            // }),
             <ArticleCard
               key={service.idservice}
               id={service.idservice}
@@ -21,9 +16,9 @@ function List({ services }) {
               category={service.categoryservice}
               image_src={service.avatarservice}
               image_alt={service.nameservice}
-              // name={UserService.username}
-              // avatar_src={UserService.avataruser}
-              // avatar_alt={UserService.username}
+              name={service.username}
+              avatar_src={service.avataruser}
+              avatar_alt={service.username}
               time={showFormattedDateTime(service.createdatservice)}
               createdAt={service.createdAt}
             />
